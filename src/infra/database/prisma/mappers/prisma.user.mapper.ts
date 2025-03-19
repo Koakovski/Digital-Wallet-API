962d3ed6c30e71cbd79bf8ab5633f933e18c39c1
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 10:44:24 -0300

    feat: added PrismaUserMapper

---
+++ src/infra/database/prisma/mappers/prisma.user.mapper.ts
@@ -0,0 +1,28 @@
+import { User } from '@prisma/client';
+import { UserEntity } from 'src/domain/entities/user.entity';
+
+export class PrismaUserMapper {
+  static toDomain(data: User): UserEntity {
+    return UserEntity.recover(
+      {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      },
+      data.id,
+    );
+  }
+
+  static toPersistence(data: UserEntity): User {
+    return {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt,
+    };
+  }
+}
