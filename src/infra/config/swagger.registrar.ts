07ad455e0b82f3f9ccca04fab41711b11a293868
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 08:41:45 -0300

    feat: added SwaggerRegistrar

---
+++ src/infra/config/swagger.registrar.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export class SwaggerRegistrar {
+  static register(app: INestApplication) {
+    const config = new DocumentBuilder()
+      .setTitle('Digital Wallet API')
+      .setDescription(
+        'API for managing balances and financial transfers between users.',
+      )
+      .setVersion('1.0')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, documentFactory);
+  }
+}
