7304a3b8cf5dd9d401eb7a987381a43d6e0f4fef
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 11:21:06 -0300

    feat: added ValidatorRegistrar

---
+++ src/infra/config/validator.registrar.ts
@@ -0,0 +1,7 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
+export class ValidatorRegistrar {
+  static register(app: INestApplication) {
+    app.useGlobalPipes(new ValidationPipe());
+  }
+}
