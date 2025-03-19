ad9fc18af8800dc1b09c5770c50d286e406b689b
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 10:45:00 -0300

    feat: apply PrismaUserMapper to PrismaUserRepository

--- src/infra/database/prisma/repositories/prisma.user.repository.ts
+++ src/infra/database/prisma/repositories/prisma.user.repository.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { UserEntity } from 'src/domain/entities/user.entity';
 import { UserRepository } from 'src/domain/repositories/user.repository';
 import { PrismaService } from '../prisma.service';
+import { PrismaUserMapper } from '../mappers/prisma.user.mapper';
 
 @Injectable()
 export class PrismaUserRepository implements UserRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(entity: UserEntity): Promise<UserEntity> {
-    // add logic to persist
-    return Promise.resolve(entity);
+  async create(entity: UserEntity): Promise<UserEntity> {
+    const prismaUser = await this.prismaService.user.create({
+      data: PrismaUserMapper.toPersistence(entity),
+    });
+
+    return PrismaUserMapper.toDomain(prismaUser);
   }
 }
