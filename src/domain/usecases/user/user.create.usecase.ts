adda752424f6b5d031e76f0d2727d41d8e7472d4
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 13:59:45 -0300

    feat: added logic to throw if user with same email already exists

--- src/domain/usecases/user/user.create.usecase.ts
+++ src/domain/usecases/user/user.create.usecase.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UserEntity } from 'src/domain/entities/user.entity';
+import { UserEmailAlreadyExistsException } from 'src/domain/exceptions/user/user.email-already-exitst.exception';
 import { UserRepository } from 'src/domain/repositories/user.repository';
 import { HashService } from 'src/domain/services/hash.service';
 
@@ -13,15 +14,30 @@ export class UserCreateUseCase {
   ) {}
 
   async execute(params: UserCreateUseCaseParams): Promise<UserEntity> {
+    await this.verifyUniqueEmail(params);
+
+    const user = await this.generateEntity(params);
+
+    return await this.userRepository.create(user);
+  }
+
+  private async verifyUniqueEmail(params: UserCreateUseCaseParams) {
+    const userWithSameEmail = await this.userRepository.findByEmail(
+      params.email,
+    );
+    if (userWithSameEmail) {
+      throw new UserEmailAlreadyExistsException();
+    }
+  }
+
+  private async generateEntity(params: UserCreateUseCaseParams) {
     const hashedPassword = await this.hashService.hash(params.password);
 
-    const user = UserEntity.new({
+    return UserEntity.new({
       name: params.name,
       email: params.email,
       password: hashedPassword,
     });
-
-    return await this.userRepository.create(user);
   }
 }
 
