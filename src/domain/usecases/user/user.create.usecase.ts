168a3a56ec3005020290a7d80069d32e4bcb081a
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 11:14:49 -0300

    feat: added UserCreateUseCase

---
+++ src/domain/usecases/user/user.create.usecase.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { UserEntity } from 'src/domain/entities/user.entity';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+
+@Injectable()
+export class UserCreateUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute(params: UserCreateUseCaseParams): Promise<UserEntity> {
+    const user = UserEntity.new({
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    });
+
+    return await this.userRepository.create(user);
+  }
+}
+
+export type UserCreateUseCaseParams = {
+  name: string;
+  email: string;
+  password: string;
+};
