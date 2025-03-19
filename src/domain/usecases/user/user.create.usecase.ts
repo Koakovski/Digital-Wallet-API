a5d4181d1c7ae1cff438b23a467917c3f6121f59
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 13:41:38 -0300

    feat: apply HashService to UserCreateUseCase

--- src/domain/usecases/user/user.create.usecase.ts
+++ src/domain/usecases/user/user.create.usecase.ts
@@ -1,19 +1,24 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UserEntity } from 'src/domain/entities/user.entity';
 import { UserRepository } from 'src/domain/repositories/user.repository';
+import { HashService } from 'src/domain/services/hash.service';
 
 @Injectable()
 export class UserCreateUseCase {
   constructor(
     @Inject(UserRepository)
     private readonly userRepository: UserRepository,
+    @Inject(HashService)
+    private readonly hashService: HashService,
   ) {}
 
   async execute(params: UserCreateUseCaseParams): Promise<UserEntity> {
+    const hashedPassword = await this.hashService.hash(params.password);
+
     const user = UserEntity.new({
       name: params.name,
       email: params.email,
-      password: params.password,
+      password: hashedPassword,
     });
 
     return await this.userRepository.create(user);
