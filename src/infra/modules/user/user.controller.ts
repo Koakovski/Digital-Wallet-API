5a70c6740e15f8074e9b30e74e91c138e5875008
Koakovskie <hkoakovskip@gmail.com>
2025-03-19 13:16:50 -0300

    feat: added presenter and doc to UserController

--- src/infra/modules/user/user.controller.ts
+++ src/infra/modules/user/user.controller.ts
@@ -1,19 +1,27 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserCreateUseCase } from 'src/domain/usecases/user/user.create.usecase';
 import { UserCreateDto } from './dtos/user.create.dto';
+import { UserPresentableEntity } from './presenters/user.presentable-entity';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserPresenter } from './presenters/user.presenter';
 
+@ApiTags('user')
 @Controller('/user')
 export class UserController {
   constructor(private readonly userCreateUseCase: UserCreateUseCase) {}
 
   @Post('/')
-  async create(@Body() body: UserCreateDto) {
+  @ApiOperation({ description: 'create a new User' })
+  @ApiCreatedResponse({
+    type: UserPresentableEntity,
+  })
+  async create(@Body() body: UserCreateDto): Promise<UserPresentableEntity> {
     const user = await this.userCreateUseCase.execute({
       name: body.name,
       email: body.email,
       password: body.password,
     });
 
-    return user;
+    return new UserPresenter(user).toPresent();
   }
 }
