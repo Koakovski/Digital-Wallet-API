d07c457c431d5b356aefa0008eda2ab1b3111d80
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 19:00:18 -0300

    feat: added findById method to UserRepository

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -2,6 +2,7 @@ import { UserEntity } from '../entities/user.entity';
 
 export interface UserRepository {
   create(entity: UserEntity): Promise<UserEntity>;
+  findById(id: string): Promise<UserEntity | null>;
   findByEmail(email: string): Promise<UserEntity | null>;
 }
 
