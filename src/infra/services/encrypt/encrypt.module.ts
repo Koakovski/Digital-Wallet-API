ff7918bb025c556c7e1a7048ebaa9a87f5cbc9d7
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 16:49:09 +0000

    feat: added EncryptModule to AppModule as Global

--- src/infra/services/encrypt/encrypt.module.ts
+++ src/infra/services/encrypt/encrypt.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { EncryptService } from 'src/domain/services/encrypt.service';
 import { JwtEncryptService } from './jwt.encrypt.service';
 
+@Global()
 @Module({
   imports: [JwtModule.register({ secret: 'secret_key' })],
   providers: [
