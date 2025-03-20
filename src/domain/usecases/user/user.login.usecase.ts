6fe7cfb1796fcf5287c6d47c93ce120018f41658
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 17:07:45 +0000

    feat: added UserLoginUseCase

---
+++ src/domain/usecases/user/user.login.usecase.ts
@@ -0,0 +1,73 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UseCase } from 'src/domain/base/usecase';
+import { UserEntity } from 'src/domain/entities/user.entity';
+import { UserNotFoundException } from 'src/domain/exceptions/user/user.not-found.exception';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+import { EncryptService } from 'src/domain/services/encrypt.service';
+import { HashService } from 'src/domain/services/hash.service';
+
+@Injectable()
+export class UserLoginUseCase
+  implements UseCase<UserLoginUseCaseParams, UserLoginUseCaseResult>
+{
+  constructor(
+    @Inject(UserRepository)
+    private readonly userRepository: UserRepository,
+    @Inject(HashService)
+    private readonly hashService: HashService,
+    @Inject(EncryptService)
+    private readonly encryptService: EncryptService,
+  ) {}
+
+  async execute(
+    params: UserLoginUseCaseParams,
+  ): Promise<UserLoginUseCaseResult> {
+    const user = await this.fetchUser(params);
+    await this.validatePassword(user, params);
+
+    const token = this.encryptService.encrypt({
+      id: user.id,
+      email: user.email,
+    });
+
+    return {
+      token,
+      user,
+    };
+  }
+
+  private async fetchUser(params: UserLoginUseCaseParams) {
+    const user = await this.userRepository.findByEmail(params.email);
+    if (!user) {
+      throw new UserNotFoundException(
+        `User with email ${params.email} was not found`,
+      );
+    }
+
+    return user;
+  }
+
+  private async validatePassword(
+    user: UserEntity,
+    params: UserLoginUseCaseParams,
+  ) {
+    const isPasswordValid = await this.hashService.compare(
+      params.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException();
+    }
+  }
+}
+
+export type UserLoginUseCaseParams = {
+  email: string;
+  password: string;
+};
+
+export type UserLoginUseCaseResult = {
+  token: string;
+  user: UserEntity;
+};
--- src/infra/usecases/user/user.usecase.module.ts
+++ src/infra/usecases/user/user.usecase.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserCreateUseCase } from 'src/domain/usecases/user/user.create.usecase';
+import { UserLoginUseCase } from 'src/domain/usecases/user/user.login.usecase';
 import { DatabaseModule } from 'src/infra/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [UserCreateUseCase],
-  exports: [UserCreateUseCase],
+  providers: [UserCreateUseCase, UserLoginUseCase],
+  exports: [UserCreateUseCase, UserLoginUseCase],
 })
 export class UserUseCaseModule {}
