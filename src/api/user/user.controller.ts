ff3466685a243cc2c3033a3bca1163f4cdb912aa
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 17:17:58 +0000

    feat: added route to user login in system

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -2,13 +2,24 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserCreateUseCase } from 'src/domain/usecases/user/user.create.usecase';
 import { UserCreateDto } from './dtos/user.create.dto';
 import { UserPresentableEntity } from './presenters/user.presentable-entity';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserPresenter } from './presenters/user.presenter';
+import { UserLoginUseCase } from 'src/domain/usecases/user/user.login.usecase';
+import { UserLoginDto } from './dtos/user.login.dto';
+import { UserLoginResponse } from './presenters/user.login-response';
 
 @ApiTags('user')
 @Controller('/user')
 export class UserController {
-  constructor(private readonly userCreateUseCase: UserCreateUseCase) {}
+  constructor(
+    private readonly userCreateUseCase: UserCreateUseCase,
+    private readonly userLoginUseCase: UserLoginUseCase,
+  ) {}
 
   @Post('/')
   @ApiOperation({ description: 'create a new User' })
@@ -23,5 +34,20 @@ export class UserController {
     });
 
     return new UserPresenter(user).toPresent();
+  }
+
+  @Post('/login')
+  @ApiOperation({ description: 'Login to the system' })
+  @ApiOkResponse({
+    type: UserLoginResponse,
+  })
+  async login(@Body() body: UserLoginDto): Promise<UserLoginResponse> {
+    const { user, token } = await this.userLoginUseCase.execute({
+      email: body.email,
+      password: body.password,
+    });
+
+    const userPresentable = new UserPresenter(user).toPresent();
+    return { token, user: userPresentable };
   }
 }
