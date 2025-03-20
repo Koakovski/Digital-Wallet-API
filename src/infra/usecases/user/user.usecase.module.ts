34ff566723945ff68831e73021b3e5d171199335
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 18:15:50 +0000

    feat: added UserFindByEmailUseCase

--- src/infra/usecases/user/user.usecase.module.ts
+++ src/infra/usecases/user/user.usecase.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserAuthorizeUseCase } from 'src/domain/usecases/user/user.authorize.usecase';
 import { UserCreateUseCase } from 'src/domain/usecases/user/user.create.usecase';
+import { UserFindByEmailUseCase } from 'src/domain/usecases/user/user.find-by-email.usecase';
 import { UserLoginUseCase } from 'src/domain/usecases/user/user.login.usecase';
 import { DatabaseModule } from 'src/infra/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [UserCreateUseCase, UserLoginUseCase, UserAuthorizeUseCase],
-  exports: [UserCreateUseCase, UserLoginUseCase, UserAuthorizeUseCase],
+  providers: [
+    UserCreateUseCase,
+    UserLoginUseCase,
+    UserAuthorizeUseCase,
+    UserFindByEmailUseCase,
+  ],
+  exports: [
+    UserCreateUseCase,
+    UserLoginUseCase,
+    UserAuthorizeUseCase,
+    UserFindByEmailUseCase,
+  ],
 })
 export class UserUseCaseModule {}
