3c61bf833a1e7ed5b953fa8ed05b2495064c0506
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 18:42:15 -0300

    refactor: remove transaction repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaUserRepository } from './prisma/repositories/prisma.user.repository';
 import { UserRepository } from 'src/domain/repositories/user.repository';
 import { PrismaService } from './prisma/prisma.service';
-import { TransactionRepository } from 'src/domain/repositories/transaction.repository';
-import { PrismaTransactionRepository } from './prisma/repositories/prisma.transaction.repository';
 
 @Module({
   providers: [
@@ -12,11 +10,7 @@ import { PrismaTransactionRepository } from './prisma/repositories/prisma.transa
       provide: UserRepository,
       useClass: PrismaUserRepository,
     },
-    {
-      provide: TransactionRepository,
-      useClass: PrismaTransactionRepository,
-    },
   ],
-  exports: [UserRepository, TransactionRepository],
+  exports: [UserRepository],
 })
 export class DatabaseModule {}
