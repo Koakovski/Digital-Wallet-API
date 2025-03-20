970118fdead6a8505f795dbeb2653c34c52af165
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 18:57:22 -0300

    refactor: refactor logic to persist TransactionAggregate

--- src/infra/database/prisma/repositories/prisma.transaction.aggregate.repository.ts
+++ src/infra/database/prisma/repositories/prisma.transaction.aggregate.repository.ts
@@ -4,6 +4,7 @@ import { TransactionAggregateRepository } from 'src/domain/repositories/transact
 import { PrismaService } from '../prisma.service';
 import { PrismaTransactionMapper } from '../mappers/prisma.transaction.mapper';
 import { PrismaTransactionAggregateMapper } from '../mappers/prisma.transaction.aggregate.mapper';
+import { PrismaUserMapper } from '../mappers/prisma.user.mapper';
 
 @Injectable()
 export class PrismaTransactionAggregateRepository
@@ -12,13 +13,31 @@ export class PrismaTransactionAggregateRepository
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(aggregate: TransactionAggregate): Promise<TransactionAggregate> {
-    const prismaTransactionData = await this.prismaService.transaction.create({
-      data: PrismaTransactionMapper.toPersistence(aggregate.root),
-      include: {
-        Sender: true,
-        Receiver: true,
+    const prismaTransactionData = await this.prismaService.$transaction(
+      async (tx) => {
+        await tx.user.update({
+          where: {
+            id: aggregate.sender.id,
+          },
+          data: PrismaUserMapper.toPersistence(aggregate.sender),
+        });
+
+        await tx.user.update({
+          where: {
+            id: aggregate.receiver.id,
+          },
+          data: PrismaUserMapper.toPersistence(aggregate.receiver),
+        });
+
+        return await tx.transaction.create({
+          data: PrismaTransactionMapper.toPersistence(aggregate.root),
+          include: {
+            Sender: true,
+            Receiver: true,
+          },
+        });
       },
-    });
+    );
 
     return PrismaTransactionAggregateMapper.toDomain(prismaTransactionData);
   }
