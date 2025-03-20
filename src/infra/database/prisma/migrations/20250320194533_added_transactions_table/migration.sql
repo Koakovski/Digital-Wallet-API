ca9762a8731c7a4ea6551b952de275f100c52436
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 19:50:06 +0000

    feat: added Transactions table

---
+++ src/infra/database/prisma/migrations/20250320194533_added_transactions_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" UUID NOT NULL,
+    "sender_id" UUID NOT NULL,
+    "receiver_id" UUID NOT NULL,
+    "value_in_cents" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
