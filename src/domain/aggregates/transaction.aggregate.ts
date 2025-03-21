98cffafd20d90ea4437a4c7e11261760abc5b586
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 22:11:57 -0300

    feat: added cancellTransaction and cancel method to TransactionAggregate

--- src/domain/aggregates/transaction.aggregate.ts
+++ src/domain/aggregates/transaction.aggregate.ts
@@ -5,6 +5,7 @@ export type TransactionAggregateProps = {
   transaction: TransactionEntity;
   sender: UserEntity;
   receiver: UserEntity;
+  cancellTransaction: TransactionEntity | null;
 };
 
 export type TransactionAggregateNewByTransferProps = {
@@ -28,6 +29,10 @@ export class TransactionAggregate {
     return this.props.receiver;
   }
 
+  get cancellTransaction() {
+    return this.props.cancellTransaction;
+  }
+
   static newByTransfer(props: TransactionAggregateNewByTransferProps) {
     props.sender.removeBalance(props.valueInCents);
     props.receiver.addBalance(props.valueInCents);
@@ -42,7 +47,27 @@ export class TransactionAggregate {
       sender: props.sender,
       receiver: props.receiver,
       transaction,
+      cancellTransaction: null,
     });
+  }
+
+  cancel() {
+    const transactionToCancell = this.props.transaction;
+    const originalReceiver = this.props.receiver;
+    const originalSender = this.props.sender;
+    const valueInCents = transactionToCancell.valueInCents;
+
+    originalReceiver.removeBalance(valueInCents);
+    originalSender.addBalance(valueInCents);
+
+    const cancellTransaction = TransactionEntity.new({
+      receiverId: originalReceiver.id,
+      senderId: originalReceiver.id,
+      valueInCents,
+    });
+
+    transactionToCancell.cancel(cancellTransaction.id);
+    this.props.cancellTransaction = cancellTransaction;
   }
 
   static recover(props: TransactionAggregateProps) {
