bb7c171767637102fa1b3719de702ca58debfe0a
Koakovskie <hkoakovskip@gmail.com>
2025-03-21 14:36:52 -0300

    refactor: refactor BaseException

--- src/domain/exceptions/exception.base.ts
+++ src/domain/exceptions/exception.base.ts
@@ -1,10 +1,20 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 
-export abstract class BaseException extends HttpException {
-  public readonly code: string;
+export abstract class BaseException extends Error {
+  public readonly _code: string;
+  public readonly _status: number;
+
+  get code() {
+    return this._code;
+  }
+
+  get status() {
+    return this._status;
+  }
 
   constructor(code: string, message: string, status: HttpStatus) {
-    super(message, status);
-    this.code = code;
+    super(message);
+    this._code = code;
+    this._status = status;
   }
 }
