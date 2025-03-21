71ff29a03b6b73cd409febcf152d5b91239baf74
Koakovskie <hkoakovskip@gmail.com>
2025-03-20 21:00:48 -0300

    feat: added Paginted and PaginationRequest

---
+++ src/domain/base/paginated.ts
@@ -0,0 +1,50 @@
+export class Paginated<T> {
+  data: T[];
+  total: number;
+  page: number;
+  perPage: number;
+  totalPages: number;
+
+  constructor(data: T[], total: number, { page, perPage }: PaginationRequest) {
+    this.data = data;
+    this.total = total;
+    this.page = page;
+    this.perPage = perPage;
+    this.totalPages = Math.ceil(total / perPage);
+  }
+}
+
+export class PaginationRequest {
+  static MAX_PER_PAGE = 50;
+
+  private _page: number;
+  private _perPage: number;
+
+  get page() {
+    return this._page;
+  }
+
+  get perPage() {
+    return this._perPage;
+  }
+
+  get skip() {
+    return (this.page - 1) * this.perPage;
+  }
+
+  private set page(page: number) {
+    this.page = Math.min(page, 1);
+  }
+
+  private set perPage(perPage: number) {
+    this._perPage = Math.max(
+      Math.min(perPage, PaginationRequest.MAX_PER_PAGE),
+      1,
+    );
+  }
+
+  constructor(page = 1, perPage = 10) {
+    this._page = page;
+    this.perPage = perPage;
+  }
+}
