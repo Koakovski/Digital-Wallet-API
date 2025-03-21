9d8762124c4d8596b0251f08b56ac8d2b5c78319
Koakovskie <hkoakovskip@gmail.com>
2025-03-21 17:33:32 -0300

    feat: added userEntityMockFactory

---
+++ test/mocks/entities/user.entity.mock.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker/.';
+import * as Factory from 'factory.ts';
+import { UserEntity } from 'src/domain/entities/user.entity';
+
+export const userEntityMockFactory = Factory.Sync.makeFactory<UserEntity>({
+  id: faker.string.uuid(),
+  name: faker.person.fullName(),
+  email: faker.internet.email(),
+  password: faker.lorem.word(),
+  balanceInCents: 0,
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.past(),
+  addBalance: () => undefined,
+  removeBalance: () => undefined,
+});
