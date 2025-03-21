1efc6c0eeb62c4309b039b17e07d0c49ddec7100
Koakovskie <hkoakovskip@gmail.com>
2025-03-21 10:20:37 -0300

    refactor: refactor hard coded secret using value of ConfigService

--- src/api/user/authorization/user.auth.strategy.ts
+++ src/api/user/authorization/user.auth.strategy.ts
@@ -3,17 +3,20 @@ import { UserRepository } from 'src/domain/repositories/user.repository';
 import { AuthenticatedUserPayload } from 'src/domain/usecases/user/user.login.usecase';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from 'src/domain/services/config.service';
 
 @Injectable()
 export class UserAuthEstrategy extends PassportStrategy(Strategy) {
   constructor(
     @Inject(UserRepository)
     private readonly userRepository: UserRepository,
+    @Inject(ConfigService)
+    private readonly configService: ConfigService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: 'secret_key',
+      secretOrKey: configService.get('JWT_SECRET'),
     });
   }
 
--- src/infra/services/encrypt/encrypt.module.ts
+++ src/infra/services/encrypt/encrypt.module.ts
@@ -2,10 +2,18 @@ import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { EncryptService } from 'src/domain/services/encrypt.service';
 import { JwtEncryptService } from './jwt.encrypt.service';
+import { ConfigService } from 'src/domain/services/config.service';
 
 @Global()
 @Module({
-  imports: [JwtModule.register({ secret: 'secret_key' })],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secretOrPrivateKey: configService.get('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [
     {
       provide: EncryptService,
