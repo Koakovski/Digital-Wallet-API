24b081c5eebfd765fad4b8136b42c5ea69433c2a
Koakovskie <hkoakovskip@gmail.com>
2025-03-21 15:17:55 -0300

    refactor: refactor validationErrors of MappedError interface

--- src/domain/config/exeception-mapped/exception-mapped.ts
+++ src/domain/config/exeception-mapped/exception-mapped.ts
@@ -3,8 +3,12 @@ export interface ErrorDetail {
   message: string;
 }
 
+export interface ValidationErrorDetail extends ErrorDetail {
+  field: string;
+}
+
 export interface MappedError extends ErrorDetail {
-  validationErrors?: ErrorDetail[];
+  validationErrors?: ValidationErrorDetail[];
 }
 
 export interface MappedErrorResponse {
